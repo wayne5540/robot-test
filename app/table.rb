e504bbded4858a532634cde0ec5dd3ce01fb140f
wayne <wayne.5540@gmail.com>
2017-03-21 20:28:26 +1100

    Implement Table#place! to save robot position.
    * Add Table#place! to save robot position into table
    * Add Table#placed? to check if robot position is the same

--- app/table.rb
+++ app/table.rb
@@ -1,8 +1,24 @@
 class Table
+  attr_accessor :robot_position
   X_LENGTH = 5.freeze
   Y_LENGTH = 5.freeze
 
+  def place!(x, y)
+    if place?(x, y)
+      @robot_position = { x: x, y: y }
+      true
+    else
+      false
+    end
+  end
+
   def place?(x, y)
-    (0...X_LENGTH).include?(x) && (0...Y_LENGTH).include?(y)
+    (0...X_LENGTH).include?(x) && (0...Y_LENGTH).include?(y) && !placed?(x, y)
+  end
+
+  private
+
+  def placed?(x, y)
+    !@robot_position.nil? && @robot_position[:x] == x && @robot_position[:y] == y
   end
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -3,6 +3,30 @@ require_relative "../app/Table"
 describe "Table" do
   let(:table) { Table.new }
 
+  describe "place!" do
+    let(:x) { 1 }
+    let(:y) { 2 }
+    subject { table.place!(x, y) }
+
+    context "when place-able" do
+      it "sets robot_position" do
+        subject
+
+        expect(table.robot_position).to eq({ x: x, y: y })
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context "when non-place-able" do
+      before do
+        allow(table).to receive(:place?).with(x, y) { false }
+      end
+
+      it { is_expected.to be false }
+    end
+  end
+
   describe "#place?" do
     describe "boundary" do
       context 'when inside boundary' do
@@ -16,6 +40,16 @@ describe "Table" do
         it { expect(table.place?(1, -1)).to be false }
         it { expect(table.place?(5, 0)).to be false }
         it { expect(table.place?(1, 5)).to be false }
+      end
+    end
+
+    describe "placed" do
+      let(:x) { 1 }
+      let(:y) { 2 }
+      before { table.robot_position = { x: x, y: y } }
+
+      it "returns false" do
+        expect(table.place?(x, y)).to be false
       end
     end
   end
