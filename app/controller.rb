51c314ebb8afe8fcaaa31db13572ba78a7aa99a4
wayne <wayne.5540@gmail.com>
2017-03-28 14:46:51 +1100

    Add outputs to controller, fix bugs
    * Replace Error raising with outputs array
    * Fix bug: passing wrong argument (nil) to Robot#face?

--- app/controller.rb
+++ app/controller.rb
@@ -2,14 +2,12 @@ require_relative "table"
 require_relative "robot"
 
 class Controller
-  class Error < StandardError; end
-  class InvalidArgumentsError < Error; end
-  class InvalidCommandError < Error; end
-  attr_reader :table, :robot
+  attr_reader :table, :robot, :outputs
 
   def initialize
     @table = Table.new
     @robot = Robot.new
+    @outputs = []
   end
 
   def execute!(command)
@@ -21,22 +19,22 @@ class Controller
       if robot.turn?
         robot.right!
       else
-        raise InvalidCommandError.new("Error: #{action} is only available after robot has been placed")
+        output!("Error: #{action} is only available after robot has been placed")
       end
     when "LEFT"
       if robot.turn?
         robot.left!
       else
-        raise InvalidCommandError.new("Error: #{action} is only available after robot has been placed")
+        output!("Error: #{action} is only available after robot has been placed")
       end
     when "MOVE"
       if robot.turn?
         move!
       else
-        raise InvalidCommandError.new("Error: #{action} is only available after robot has been placed")
+        output!("Error: #{action} is only available after robot has been placed")
       end
     else
-      raise InvalidCommandError.new("Error: #{action} is not a valid action")
+      output!("Error: #{action} is not a valid action")
     end
   end
 
@@ -54,14 +52,23 @@ class Controller
   end
 
   def place!(arguments)
-    raise InvalidArgumentsError.new("PLACE action requires 3 arguments: X,Y,F") if arguments.nil?
+    if arguments.nil?
+      output!("Error: PLACE action requires 3 arguments: X,Y,F")
+      return
+    end
     x, y, face = arguments.split(",", 3)
     x, y = [x.to_i, y.to_i]
 
     # TODO: show error message if not able to place
-    if robot.face?(face) && table.place?(x, y)
+    if robot.face?(face.to_s) && table.place?(x, y)
       robot.face = face
       table.place!(x, y)
+    else
+      output!("Error: InvalidArgumentsError")
     end
+  end
+
+  def output!(message)
+    outputs << message
   end
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -7,13 +7,22 @@ describe "controller" do
     describe "action PLACE" do
       context "when invalid arguments" do
         specify do
-          expect { controller.execute!("PLACE") }.to raise_error Controller::InvalidArgumentsError
+          controller.execute!("PLACE")
+
+          expect(controller.outputs.first).to eq("Error: PLACE action requires 3 arguments: X,Y,F")
+        end
+
+        specify do
+          controller.execute!("PLACE one,")
+
+          expect(controller.outputs.first).to eq("Error: InvalidArgumentsError")
         end
 
         context "when face is invalid" do
           it "doesn't change robot face orientation" do
             expect { controller.execute!("PLACE 1,2,SOUTHWEST") }.not_to change { controller.robot.face }
           end
+
           it "doesn't change robot position" do
             expect { controller.execute!("PLACE 1,2,SOUTHWEST") }.not_to change { controller.table.robot_position }
           end
@@ -23,6 +32,7 @@ describe "controller" do
           it "doesn't change robot face orientation" do
             expect { controller.execute!("PLACE -1,2,NORTH") }.not_to change { controller.robot.face }
           end
+
           it "doesn't change robot position" do
             expect { controller.execute!("PLACE -1,2,NORTH") }.not_to change { controller.table.robot_position }
           end
@@ -39,7 +49,9 @@ describe "controller" do
     describe "action LEFT" do
       context "when robot has not been placed yet" do
         specify do
-          expect { controller.execute!("LEFT") }.to raise_error Controller::InvalidCommandError
+          controller.execute!("LEFT")
+
+          expect(controller.outputs.first).to include("Error:")
         end
       end
 
@@ -58,7 +70,9 @@ describe "controller" do
     describe "action RIGHT" do
       context "when robot has not been placed yet" do
         specify do
-          expect { controller.execute!("RIGHT") }.to raise_error Controller::InvalidCommandError
+          controller.execute!("RIGHT")
+
+          expect(controller.outputs.first).to include("Error:")
         end
       end
 
@@ -77,7 +91,9 @@ describe "controller" do
     describe "action MOVE" do
       context "when robot has not been placed yet" do
         specify do
-          expect { controller.execute!("MOVE") }.to raise_error Controller::InvalidCommandError
+          controller.execute!("MOVE")
+
+          expect(controller.outputs.first).to include("Error:")
         end
       end
 
@@ -145,11 +161,15 @@ describe "controller" do
 
     describe "invalid action" do
       specify "case sensitive" do
-        expect { controller.execute!("right") }.to raise_error Controller::InvalidCommandError
+        controller.execute!("right")
+
+        expect(controller.outputs.first).to eq("Error: right is not a valid action")
       end
 
       specify do
-        expect { controller.execute!("invalid command") }.to raise_error Controller::InvalidCommandError
+        controller.execute!("invalid command")
+
+        expect(controller.outputs.first).to eq("Error: invalid is not a valid action")
       end
     end
   end
