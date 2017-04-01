dfc87acf06954bce63377b138c79a2dba074e011
wayne <wayne.5540@gmail.com>
2017-04-01 23:26:29 +1100

    Replace Robot::ROTATION with Robot#rotation

--- app/robot.rb
+++ app/robot.rb
@@ -1,27 +1,12 @@
 class Robot
   attr_accessor :face
 
-  ROTATION = {
-    north: {
-      left: :west, right: :east
-    },
-    west: {
-      left: :south, right: :north
-    },
-    south: {
-      left: :east, right: :west
-    },
-    east: {
-      left: :north, right: :south
-    }
-  }.freeze
-
   def face=(orientation)
     @face = downcase_symbol(orientation) if face?(orientation)
   end
 
   def face?(orientation)
-    !ROTATION[downcase_symbol(orientation)].nil?
+    !rotation[downcase_symbol(orientation)].nil?
   end
 
   def orientation_set?
@@ -29,14 +14,31 @@ class Robot
   end
 
   def left!
-    @face = ROTATION[face][:left] if orientation_set?
+    @face = rotation[face][:left] if orientation_set?
   end
 
   def right!
-    @face = ROTATION[face][:right] if orientation_set?
+    @face = rotation[face][:right] if orientation_set?
   end
 
   private
+
+  def rotation
+    @rotation ||= {
+      north: {
+        left: :west, right: :east
+      },
+      west: {
+        left: :south, right: :north
+      },
+      south: {
+        left: :east, right: :west
+      },
+      east: {
+        left: :north, right: :south
+      }
+    }
+  end
 
   def downcase_symbol(string)
     string.downcase.to_sym
